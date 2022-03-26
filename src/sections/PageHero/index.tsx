71f0dc0f4e9b502fd1b2012d2239e5809dee958a
apthos <david00lara@gmail.com>
2022-03-26 05:46:41 -0400

    Added hacakthon details to page hero

--- src/sections/PageHero/index.tsx
+++ src/sections/PageHero/index.tsx
@@ -4,18 +4,27 @@ import LinkButton from "../../components/LinkButton";
 
 const PageHero: React.FC = () => {
     return (
-        <section className="background">
-            <div className="buttonsDiv">
-                <LinkButton text="Register Now!" url="" filled={true} />
-                <div className="buttonsRow">
-                    <LinkButton
-                        text="Discord"
-                        url="https://discord.gg/upefiu"
-                    />
-                    <LinkButton
-                        text="Sponsor Us"
-                        url="mailto:Upe@fiu.edu?subject=We want to sponsor"
-                    />
+        <section className="pageHeroBackground">
+            <div className="pageHeroMiddle">
+                <div className="detailsDiv">
+                    <ul>
+                        <li>September 23-25, 2022</li>
+                        <li>Florida International University</li>
+                        <li>Miami, FL ● In-Person & Virtual!</li>
+                    </ul>
+                </div>
+                <div className="buttonsDiv">
+                    <LinkButton text="Register Now!" url="" filled={true} />
+                    <div className="buttonsRow">
+                        <LinkButton
+                            text="Discord"
+                            url="https://discord.gg/upefiu"
+                        />
+                        <LinkButton
+                            text="Sponsor Us"
+                            url="mailto:Upe@fiu.edu?subject=We want to sponsor"
+                        />
+                    </div>
                 </div>
             </div>
         </section>
