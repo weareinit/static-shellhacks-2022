1119f785a1ba29ea9d02f29c017ed0f213361939
apthos <david00lara@gmail.com>
2022-03-26 03:53:38 -0400

    Added simple LinkButton hover effect

--- src/components/LinkButton/index.tsx
+++ src/components/LinkButton/index.tsx
@@ -10,6 +10,11 @@ type ButtonProps = {
 const LinkButton: React.FC<ButtonProps> = (props: ButtonProps) => {
     return (
         <a href={props.url} className="button">
+            <div className="buttonBackgroundContainer">
+                <div className="defaultGradient" />
+                <div className="mediumGradient" />
+                <div className="smallGradient" />
+            </div>
             <span className={props.filled ? "filled" : "transparent"}>
                 {props.text}
             </span>
