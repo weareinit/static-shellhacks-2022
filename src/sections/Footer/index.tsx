4cd7d00e9e9fd612ffce44f5d519550ef63e17d3
Carlos Chavez <cenric09@gmail.com>
2022-04-07 16:58:50 -0400

    Starting application form, design and validation are still WIP.

--- src/sections/Footer/index.tsx
+++ src/sections/Footer/index.tsx
@@ -4,32 +4,29 @@ import { Emoji, EmojiProvider } from "react-apple-emojis";
 import emojiData from "react-apple-emojis/lib/data.json";
 import SocialButton from "../../components/SocialButton";
 import { SocialType } from "../../util/types";
+import { Link } from "react-router-dom";
 
 const Footer: React.FC = () => {
-    return (
-        <section className="footerBackground">
-            <div className="footerText">
-                {/* prettier-ignore */}
-                <p>
+  return (
+    <section className="footerBackground">
+      <div className="footerText">
+        {/* prettier-ignore */}
+        <p>
                     Made with <EmojiProvider data={emojiData}><Emoji className="yellowHeart" name="yellow-heart"/></EmojiProvider> from Upsilon Pi Epsilon
                 </p>
-                <a
-                    href="http://mlh.io/code-of-conduct"
-                    target="_blank"
-                    rel="noreferrer noopoener"
-                >
-                    MLH Code of Conduct
-                </a>
-            </div>
-            <div className="footerSocial">
-                <SocialButton type={SocialType.FACEBOOK} />
-                <SocialButton type={SocialType.TWITTER} />
-                <SocialButton type={SocialType.INSTAGRAM} />
-                <SocialButton type={SocialType.LINKEDIN} />
-                <SocialButton type={SocialType.YOUTUBE} />
-            </div>
-        </section>
-    );
+        <a href="http://mlh.io/code-of-conduct" target="_blank" rel="noreferrer noopoener">
+          MLH Code of Conduct
+        </a>
+      </div>
+      <div className="footerSocial">
+        <SocialButton type={SocialType.FACEBOOK} />
+        <SocialButton type={SocialType.TWITTER} />
+        <SocialButton type={SocialType.INSTAGRAM} />
+        <SocialButton type={SocialType.LINKEDIN} />
+        <SocialButton type={SocialType.YOUTUBE} />
+      </div>
+    </section>
+  );
 };
 
 export default Footer;
--- src/sections/PageHero/index.tsx
+++ src/sections/PageHero/index.tsx
@@ -6,43 +6,33 @@ import LinkButton from "../../components/LinkButton";
 import ShellHacks from "../../svg/ShellHacks.svg";
 
 const PageHero: React.FC = () => {
-    return (
-        <section className="pageHeroBackground">
-            <div className="pageHeroTop">
-                <img
-                    alt="ShellHacks Logo"
-                    className="pageHeroLogo"
-                    src={ShellHacks}
-                />
-                <h2>FLORIDA'S LARGEST HACKATHON</h2>
-            </div>
-            <div className="pageHeroMiddle">
-                <div className="detailsDiv">
-                    {/* prettier-ignore */}
-                    <EmojiProvider data={emojiData}>
+  return (
+    <section className="pageHeroBackground">
+      <div className="pageHeroTop">
+        <img alt="ShellHacks Logo" className="pageHeroLogo" src={ShellHacks} />
+        <h2>FLORIDA'S LARGEST HACKATHON</h2>
+      </div>
+      <div className="pageHeroMiddle">
+        <div className="detailsDiv">
+          {/* prettier-ignore */}
+          <EmojiProvider data={emojiData}>
                         <ul>
                             <li><Emoji className="detailsMarker" name="rocket"/>September 23-25, 2022</li>
                             <li><Emoji className="detailsMarker" name="rocket"/>Florida International University</li>
                             <li><Emoji className="detailsMarker" name="rocket"/>Miami, FL ● In-Person & Virtual!</li>
                         </ul>
                     </EmojiProvider>
-                </div>
-                <div className="buttonsDiv">
-                    <LinkButton text="Register Now!" url="" filled={true} />
-                    <div className="buttonsRow">
-                        <LinkButton
-                            text="Discord"
-                            url="https://discord.gg/upefiu"
-                        />
-                        <LinkButton
-                            text="Sponsor Us"
-                            url="mailto:Upe@fiu.edu?subject=We want to sponsor"
-                        />
-                    </div>
-                </div>
-            </div>
-        </section>
-    );
+        </div>
+        <div className="buttonsDiv">
+          <LinkButton text="Register Now!" url="/application" filled={true} />
+          <div className="buttonsRow">
+            <LinkButton text="Discord" url="https://discord.gg/upefiu" />
+            <LinkButton text="Sponsor Us" url="mailto:Upe@fiu.edu?subject=We want to sponsor" />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default PageHero;
