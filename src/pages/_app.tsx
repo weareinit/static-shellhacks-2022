371599ac66884942c90d8e3306d69b59518210e1
apthos <david00lara@gmail.com>
2022-06-19 14:36:42 -0700

    Heroku'd

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import "./index.css";
 import type { AppProps } from "next/app";
 import initAuth from "../util/initAuth"; // the module you created above
 import { app } from "../server/firebaseApp";
+import path from "path";
 
 app;
 initAuth();
