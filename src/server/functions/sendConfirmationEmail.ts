58ac1605d9c1f6be2dd1ef120752acf2b9c51be8
apthos <david00lara@gmail.com>
2022-08-21 21:07:32 -0400

    Updated confirmation email

--- src/server/functions/sendConfirmationEmail.ts
+++ src/server/functions/sendConfirmationEmail.ts
@@ -1,4 +1,5 @@
 import { addDoc, collection } from "firebase/firestore";
+import path from "path";
 import { db } from "../firebaseApp";
 
 export async function sendConfirmationEmail(
@@ -6,6 +7,16 @@ export async function sendConfirmationEmail(
     name: string,
     attendance: string
 ): Promise<boolean> {
+    const square = path.join(
+        process.cwd(),
+        "public/static",
+        "Accepted_Square.png"
+    );
+    const rectangle = path.join(
+        process.cwd(),
+        "public/static",
+        "Accepted_Rectangle.png"
+    );
     const location = attendance === "Remote" ? "VIRTUAL" : "IN-PERSON";
     const address =
         attendance === "Remote"
@@ -21,7 +32,7 @@ export async function sendConfirmationEmail(
 
             Please be sure to do the following prior to the event!
             
-            1️⃣ Check out the ShellHacks FAQ
+            1️⃣ Check Out the ShellHacks FAQ
             
             You can find an informational guide to help answer some questions and get you ready for ShellHacks at https://upefiu.notion.site/ShellHacks-2022-Applicant-Updates-b7b549e525164c4b935f5e3126565946.
             
@@ -29,10 +40,10 @@ export async function sendConfirmationEmail(
             
             Discord is our official communication platform for ShellHacks, so be sure to sign up for it as soon as you can! On Discord you can chat with fellow attendees, discuss project ideas, find team members, get updates and reminders on certain events during Shellhacks, and more. You can also contact the event organizers and ask us any questions you have! Join our Discord using this link: https://discord.gg/upefiu.
             
-            3️⃣ Follow us on Instagram
+            3️⃣ Follow Us and Share Your Acceptance
             
-            Let everyone know you're excited for ShellHacks! Follow us on Instagram at https://www.instagram.com/upefiu and make a post or story tagging @upefiu using the hashtag #ShellHacks and please also keep an eye on our social media @upefiu for any future updates!
-            
+            Let everyone know you're excited for ShellHacks! Follow us on Instagram at https://www.instagram.com/upefiu and Twitter at https://twitter.com/upefiu and make a post or story tagging @upefiu using the hashtag #ShellHacks — attached below are some images for you to use! Please check us out on LinkedIn at https://www.linkedin.com/company/28673457 and Facebook at https://www.facebook.com/upefiu too and keep an eye out for any future updates!
+
             Best,
             
             ShellHacks Organizing Team`,
@@ -44,15 +55,25 @@ export async function sendConfirmationEmail(
                     : "<p>The address for the in-person event is:<br>Kovens Conference Center<br>FIU Biscayne Bay Campus<br>3000 NE 151st St, North Miami, FL 33181</p>"
             }
             <p><u>Please be sure to do the following prior to the event!</u></p>
-            <p><b>1️⃣ Check out the ShellHacks FAQ</b></p>
+            <p><b>1️⃣ Check Out the ShellHacks FAQ</b></p>
             <p><a href="https://upefiu.notion.site/ShellHacks-2022-Applicant-Updates-b7b549e525164c4b935f5e3126565946" target="_blank" rel="noopener noreferrer">Here</a> is an informational guide to help answer some questions and get you ready for ShellHacks.</p>
             <p><b>2️⃣ Join Our Discord</b></p>
             <p>Discord is our official communication platform for ShellHacks, so be sure to sign up for it as soon as you can! On Discord you can chat with fellow attendees, discuss project ideas, find team members, get updates and reminders on certain events during Shellhacks, and more. You can also contact the event organizers and ask us any questions you have! Join our Discord <a href="https://discord.gg/upefiu" target="_blank" rel="noopener noreferrer">here</a>.</p>
-            <p><b>3️⃣ Follow us on Instagram</b></p>
-            <p>Let everyone know you're excited for ShellHacks! Follow us on <a href="https://www.instagram.com/upefiu" target="_blank" rel="noopener noreferrer">Instagram</a> and make a post or story tagging @upefiu using the hashtag #ShellHacks and please also keep an eye on our social media @upefiu for any future updates!</p>
+            <p><b>3️⃣ Follow Us and Share Your Acceptance</b></p>
+            <p>Let everyone know you're excited for ShellHacks! Follow us on <a href="https://www.instagram.com/upefiu" target="_blank" rel="noopener noreferrer">Instagram</a> and <a href="https://twitter.com/upefiu" target="_blank" rel="noopener noreferrer">Twitter</a> and make a post or story tagging @upefiu using the hashtag #ShellHacks — attached below are some images for you to use! Please check us out on <a href="https://www.linkedin.com/company/28673457" target="_blank" rel="noopener noreferrer">LinkedIn</a> and <a href="https://www.facebook.com/upefiu" target="_blank" rel="noopener noreferrer">Facebook</a> too and keep an eye out for any future updates!</p>
             <br>
             <p>With ❤️,</p>
             <p>ShellHacks Organizing Team</p>`,
+            attachments: [
+                {
+                    filename: "ShellHacks_Accepted_Square.png",
+                    path: square,
+                },
+                {
+                    filename: "ShellHacks_Accepted_Rectangle.png",
+                    path: rectangle,
+                },
+            ],
         },
     };
 
