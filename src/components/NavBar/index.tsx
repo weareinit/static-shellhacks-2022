554ffcc0f7c9029c2f840a23b6d1f8edcca811d9
ZackarySantana <zackzackyack@gmail.com>
2022-08-30 12:54:50 -0400

    Added to nav

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -46,7 +46,7 @@ const NavBar: React.FC<NavBarProps> = (props: NavBarProps) => {
     const showLogo = isInLandingPage ? !isAtTop : true;
 
     let accountActionText = "Text";
-    let accountAction: () => void = () => {};
+    let accountAction: () => void = () => { };
     let hideAccountAction = false;
 
     switch (props.accountAction) {
@@ -87,9 +87,8 @@ const NavBar: React.FC<NavBarProps> = (props: NavBarProps) => {
                 />
                 {/** MOBILE NAVBAR DROPDOWN */}
                 <div
-                    className={`${styles.dropdown} ${
-                        hideAccountAction ? styles.accountActionHidden : ""
-                    } `}
+                    className={`${styles.dropdown} ${hideAccountAction ? styles.accountActionHidden : ""
+                        } `}
                 >
                     {showDropdown ? (
                         <ArrowUp
@@ -104,16 +103,14 @@ const NavBar: React.FC<NavBarProps> = (props: NavBarProps) => {
                     )}
 
                     <div
-                        className={`${styles.dropdownContent} ${
-                            !showDropdown ? styles.dropdownContentHidden : ""
-                        }`}
+                        className={`${styles.dropdownContent} ${!showDropdown ? styles.dropdownContentHidden : ""
+                            }`}
                     >
                         <div
-                            className={`${styles.dropdownItem} ${
-                                hideAccountAction
-                                    ? styles.accountActionHidden
-                                    : ""
-                            }`}
+                            className={`${styles.dropdownItem} ${hideAccountAction
+                                ? styles.accountActionHidden
+                                : ""
+                                }`}
                             onClick={accountAction}
                         >
                             <p className={styles.dropdownItemText}>
@@ -125,9 +122,17 @@ const NavBar: React.FC<NavBarProps> = (props: NavBarProps) => {
                 {/** DESKTOP NAVBAR INLINE */}
                 <div className={styles.inline}>
                     <div
-                        className={`${styles.accountAction} ${
-                            hideAccountAction ? styles.accountActionHidden : ""
-                        }`}
+                        className={`${styles.faqPage} ${hideAccountAction ? styles.accountActionHidden : ""
+                            }`}
+                        onClick={() => { router.push("/faq"); }}
+                    >
+                        <p className={styles.accountActionText}>
+                            FAQ
+                        </p>
+                    </div>
+                    <div
+                        className={`${styles.accountAction} ${hideAccountAction ? styles.accountActionHidden : ""
+                            }`}
                         onClick={accountAction}
                     >
                         <p className={styles.accountActionText}>
