275283ef2911e075839c2b619b8bef5d5ed75e12
ZackarySantana <zackzackyack@gmail.com>
2022-09-06 14:17:19 -0400

    Removed welcome to our first

--- src/sections/aboutus.tsx
+++ src/sections/aboutus.tsx
@@ -7,9 +7,6 @@ const Aboutus: React.FC = () => (
         <div>
             <div className={styles.buttonOutlinedBackground} />
             <p>Ready to kickstart your career in tech? Join us for ShellHacks, Florida's Largest Hackathon! 1,000+ students from around the world will come together to learn the latest technologies, develop innovative solutions, network with top companies, and more! Explore the world of technology through our workshop tracks. Work together with fellow students and mentors to build exciting projects. Meet recruiters and land internships and job opportunities. Win amazing prizes, get tons of cool swag, and more - all at ShellHacks!</p>
-            <p>
-                Welcome to our first
-            </p>
         </div>
     </section>
 );
